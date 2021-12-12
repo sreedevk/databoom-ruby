49340ed3490f248e354b0c8f89f32f23b40e2de0
sreedev <sreedevpadmakumar@gmail.com>
2021-12-12 09:20:33 +0530

    added neighbors + neighboring indices to matrix

--- lib/databoom/matrix.rb
+++ lib/databoom/matrix.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 require 'matrix'
-
 module Databoom
   class Matrix < ::Matrix
-    def neighbors(x, y, depth = 1); end
+    def neighbors(x, y, depth = 1)
+      neighboring_indices(x, y, depth).map do |depth_neighbor_indices|
+        depth_neighbor_indices.map do |nx, ny|
+          self[nx, ny]
+        end
+      end
+    end
 
     def neighboring_indices(x, y, depth = 1)
       depth.times.map do |cdepth|
-        neighboring_indices_at_depth(x, y, cdepth).filter do |coords|
+        neighboring_indices_at_depth(x, y, cdepth.next).filter do |coords|
           coords.none?(&:negative?) && coords.all? { |coord| coord < rank }
         end
       end
